75c000eaf48df9d4e706c8ce905e5831e66f59af
LiLQD <luuquangdung1511@gmail.com>
2026-05-09 20:24:04 +0700

    feat(ui): implement main section DOM rendering

--- src/render.js
+++ src/render.js
@@ -1,6 +1,6 @@
 export function buildLayout() {
   buildSidebar();
-  
+  buildMain()
 }
 
 function buildSidebar() {
@@ -41,5 +41,46 @@ function buildSidebar() {
   sidebar.appendChild(sidebarSection);
   sidebar.appendChild(addProject);
   document.body.appendChild(sidebar);
+}
+
+function buildMain(){
+    const main = document.createElement("main");
+  main.classList.add("main");
+
+  const mainHeader = document.createElement("div");
+  mainHeader.classList.add("main-header");
+
+  const projectTitle = document.createElement("h2");
+  projectTitle.classList.add("project-title");
+  projectTitle.id = "project-title";
+  projectTitle.textContent = "Inbox";
+
+  const addTodo = document.createElement("button");
+  addTodo.classList.add("btn-add-todo");
+  addTodo.id = "btn-add-todo";
+  addTodo.textContent = "+ Add Todo";
+
+  const todoList = document.createElement("ul");
+  todoList.id = "todo-list";
+
+  const emptyState = document.createElement("div");
+  emptyState.classList.add("empty-state");
+  emptyState.id = "empty-state";
+  
+  const emptyText = document.createElement("p");
+  emptyText.classList.add("empty-text");
+  emptyText.textContent = "Nothing here yet";
+  
+  const emptySub = document.createElement("p");
+  emptySub.classList.add("empty-sub");
+  emptySub.textContent = "Add a todo to get started";
+
+  emptyState.appendChild(emptyText);
+  emptyState.appendChild(emptySub);
+  mainHeader.appendChild(projectTitle);
+  mainHeader.appendChild(addTodo);
+  main.appendChild(mainHeader);
+  false? main.appendChild(todoList) : main.appendChild(emptyState);
+  document.body.appendChild(main);
 }
 
